4256038d8a1d9e97e1efada1e8123b8134f82959
$.Wendel <thaleswndl@gmail.com>
2024-04-18 11:49:42 -0300

    feat: Implementa componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { Post } from "./Post"
-import { Header } from "./components/Header"
+import { Header } from "./components/Header/Header"
+import { Sidebar } from "./components/Sidebar/Sidebar"
+
+import styles from "./App.module.css"
 import "./global.css"
 
 export function App() {
   return (
     <div>
-
       <Header />
 
-      <Post
-        author="Thales Wendel"
-        content="Lorem, ipsum dolor"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
 
-      <Post
-        author="Diego Fernandes"
-        content="Lorem, ipsum dolor"
-      />
+        <main>
+          <Post
+            author="Thales Wendel"
+            content="Lorem, ipsum dolor"
+          />
+
+          <Post
+            author="Diego Fernandes"
+            content="Lorem, ipsum dolor"
+          />
+        </main>
+      </div>
     </div>
   )
 }
